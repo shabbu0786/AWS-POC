3082b519faf1ffe29be659680b9e6fc043e5e227
shabbu0786 <shabana1317@gmail.com>
2024-04-30 20:41:11 -0600

    terraform version update

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,11 +1,11 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
+# terraform {
+#   required_providers {
+#     aws = {
+#       source  = "hashicorp/aws"
+#       version = "~> 5.0"
+#     }
+#   }
+# }
 
 # Configure the AWS Provider
 provider "aws" {
