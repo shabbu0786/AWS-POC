d381687eede8875404df3fd5e79706baeac6771d
shabbu0786 <shabana1317@gmail.com>
2024-04-30 20:57:03 -0600

    updated variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,12 +1,12 @@
 variable "ami_value" {
 
-  default  = "ami-04b70fa74e45c3917"
+  default = "ami-04b70fa74e45c3917"
 
 }
 
 variable "instance_type" {
-    default = "t2.micro"
-    
+  default = "t2.micro"
+
 }
 
 variable "aws_region" {
