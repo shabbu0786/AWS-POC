2434b5ef1fa5b87196db3a3ef92cba315af53ca7
shabbu0786 <shabana1317@gmail.com>
2024-04-30 22:35:25 -0600

    commiiitt

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,10 +1,10 @@
-# terraform {
-#   backend "s3" {
-#     region = "us-east-1"
-#     bucket = "buckettostoretfstatefile"
-#     key    = "terraform.tfstate"
-#     }
-# }
+terraform {
+  backend "s3" {
+    region = "us-east-1"
+    bucket = "tfbucketstate"
+    key    = "terraform.tfstate"
+    }
+}
 
 # Configure the AWS Provider
 provider "aws" {
